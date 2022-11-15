26119119989e630f6ecf9983168771ec9cc16d58
wetsummer <kimgeonwoo@uos.ac.kr>
2022-11-15 20:52:24 +0900

    MainPage Routing + App.yaml for Goodle Cloud

--- Mr_daebak-master 2/prototype_01/src/App.js
+++ Mr_daebak-master 2/prototype_01/src/App.js
@@ -1,36 +1,21 @@
-import React, {Component} from "react";
+import React from "react";
 import styled from "styled-components";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 //Page
-// import MainPage from "./component/page/MainPage";
-// import EmployeePage from "./component/page/EmployeePage";
-// import Login from './component/modal/LogIn';
+import MainPage from "./component/page/MainPage";
+import EmployeePage from "./component/page/EmployeePage";
 //css
 import "./component/ui/Common.css"
 
-// 리액트 라우터 돔에서 필요한 기능들을 불러오기.
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
-
-// 컴포넌트들을 불러오기.
-import MainPage from './component/page/MainPage';
-import LoginPage from './component/page/LoginPage';
-import RegisterPage from './component/page/RegisterPage';
 
 function App() {
-
-  // 전체 컴포넌트를 Router 태그 안에 넣어주고 Route들을 Switch 태그로 감싸준다.
-
   return (
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<MainPage/>} />
-					<Route path="/login" element={<LoginPage/>} />
-					<Route path="/register" element={<RegisterPage/>} />
-				</Routes>
-			</BrowserRouter>
-	);
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
-
-
-
--- Mr_daebak-master 2/prototype_01/src/index.js
+++ Mr_daebak-master 2/prototype_01/src/index.js
@@ -4,43 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-
-// 리덕스에서 필요한 미들웨어
-import promiseMiddleware from 'redux-promise';
-import ReduxThunk from 'redux-thunk';
-
-// reducer 불러오기
-import Reducer from './_reducers';
-
-// 미들웨어를 적용한 redux store를 만드는 과정
-const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider
-      store={createStoreWithMiddleware(Reducer,
-        /* 크롬 브라우저에서 디버깅 도구를 이용할 수 있도록 하는 도구 */
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-      )}>
-        <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     {/* Provider를 이용해 리덕스 적용 */}
-    
-//   </React.StrictMode>,
-//   document.getElementById('root'),
-// );
-
-
-
-
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
